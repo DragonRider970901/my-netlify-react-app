1cae941fd68d92f33e5b667ca97f68548940556d
Inana Susa <inana.programming@gmail.com>
2023-09-28 12:34:43 +0300

    Add new files and created Route elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, RouterProvider } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import About from './About';
+import SignUp from './SignUp';
+import Articles from './Articles';
+import Categories from './Categories';
+import Profile from './Profile';
 
 function App() {
-  //const router;
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<Root />} >
+      <Route path='/about' element={<About />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/articles' element={<Articles />} />
+      <Route path='/categories' element={<Categories />} />
+      <Route path='/profile' element={<Profile/>} />
+    </Route>
+  ));
   return (
     <div className="App">
       <RouterProvider router={router}/>
